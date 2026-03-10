fa561cecd710bc70a2ff474f5637a545998d0c6e
WaldoGames <bas.zwegers@gmail.com>
2026-03-10 12:25:27 +0100

    Add project files.

---
+++ EmployeePortalBackend/Model/CustomerSenstive.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeePortalBackend.Model
+{
+    public class CustomerSenstive
+    {
+        public string ParentId { get; set; }
+        public string Adress;
+        public string PhoneNumber;
+        //public ??? Id TO DO FIGUREOUT HOW TO HANDEL ID.
+    }
+}
