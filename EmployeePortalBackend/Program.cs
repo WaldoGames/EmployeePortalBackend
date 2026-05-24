fb8e8f5fb1e75d71b6505735cfa8f2ce304c302f
WaldoGames <48773478+WaldoGames@users.noreply.github.com>
2026-05-25 00:57:43 +0200

    small fix

--- EmployeePortalBackend/Program.cs
+++ EmployeePortalBackend/Program.cs
@@ -151,7 +151,7 @@ builder.Services.AddAuthentication(options =>
 
         options.RequireHttpsMetadata = false;
 
-        options.Authority = "https://keycloak:7443/realms/Employee";
+        options.Authority = "https://10.10.10.103:7443/realms/Employee";
 
         options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
         {
@@ -161,7 +161,7 @@ builder.Services.AddAuthentication(options =>
             ValidateIssuerSigningKey = true,
             ValidIssuers = new[]
             {
-                "https://localhost:7443/realms/Employee",
+                "https://10.10.10.103:7443/realms/Employee",
                 "https://keycloak:7443/realms/Employee"
             },
         };
